daf12bd585766031da249255daa7bea6955d8c03
Arivan <arivanamin@gmail.com>
2018-01-05 09:21:34 +0300

    complete system details pane

--- src/main/java/com/arivan/amin/widget/system/details/SystemDetailsPane.java
+++ src/main/java/com/arivan/amin/widget/system/details/SystemDetailsPane.java
@@ -1,9 +1,11 @@
 package com.arivan.amin.widget.system.details;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Logger;
@@ -17,10 +19,17 @@ public class SystemDetailsPane extends Pane
     {
         super();
         systemDetails = LinuxSystemDetails.newInstance();
-        VBox mainVBox = new VBox(10);
-        mainVBox.setAlignment(Pos.CENTER);
-        mainVBox.prefWidthProperty().bind(container.widthProperty());
-        mainVBox.prefHeightProperty().bind(container.heightProperty());
+        HBox mainHBox = new HBox(10);
+        VBox iconVBox = new VBox();
+        iconVBox.setAlignment(Pos.CENTER);
+        ImageView icon = new ImageView(new Image("linux2.png"));
+        icon.setPreserveRatio(true);
+        iconVBox.getChildren().add(icon);
+        VBox detailsVBox = new VBox();
+        detailsVBox.setPadding(new Insets(10));
+        detailsVBox.setAlignment(Pos.CENTER);
+        mainHBox.prefWidthProperty().bind(container.widthProperty());
+        mainHBox.prefHeightProperty().bind(container.heightProperty());
         Label systemNameLabel = new Label(systemDetails.systemName());
         systemNameLabel.getStyleClass().add("system-detail-label");
         Label osLabel = new Label(systemDetails.operatingSystemName() + "  " +
@@ -30,8 +39,15 @@ public class SystemDetailsPane extends Pane
         archLabel.getStyleClass().add("system-detail-label");
         Label homeLabel = new Label(systemDetails.systemHomeUrl());
         homeLabel.getStyleClass().add("system-detail-label");
-        mainVBox.getChildren().addAll(systemNameLabel, archLabel, osLabel, homeLabel);
-        getChildren().add(mainVBox);
+        detailsVBox.getChildren().addAll(systemNameLabel, archLabel, osLabel, homeLabel);
+        mainHBox.getChildren().addAll(iconVBox, detailsVBox);
+        iconVBox.prefWidthProperty().bind(mainHBox.widthProperty().multiply(0.25));
+        detailsVBox.prefWidthProperty().bind(mainHBox.widthProperty().multiply(0.75));
+        iconVBox.prefHeightProperty().bind(mainHBox.heightProperty());
+        detailsVBox.prefHeightProperty().bind(mainHBox.heightProperty());
+        icon.fitWidthProperty().bind(iconVBox.widthProperty().multiply(0.95));
+        icon.fitHeightProperty().bind(iconVBox.heightProperty().multiply(0.95));
+        getChildren().add(mainHBox);
     }
     
     @NotNull
