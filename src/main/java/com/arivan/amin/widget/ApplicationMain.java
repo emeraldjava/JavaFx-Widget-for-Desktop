960912a074c1d579995aa9b7fd5d15143ae925d0
Arivan <arivanamin@gmail.com>
2018-01-19 18:12:28 +0300

    refactor main class

--- src/main/java/com/arivan/amin/widget/ApplicationMain.java
+++ src/main/java/com/arivan/amin/widget/ApplicationMain.java
@@ -10,8 +10,10 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+@SuppressWarnings ({ "ClassHasNoToStringMethod", "PublicConstructor", "ClassWithoutLogger" })
 public class ApplicationMain extends Application
 {
+    @SuppressWarnings ("InstanceVariableOfConcreteClass")
     private PropertiesManager properties;
     private Scene scene;
     
@@ -20,55 +22,80 @@ public class ApplicationMain extends Application
         launch(args);
     }
     
+    @SuppressWarnings ("PublicMethodNotExposedInInterface")
     @Override
     public void start (Stage primaryStage)
     {
         properties = PropertiesManager.newInstance();
-        primaryStage.initStyle(StageStyle.UTILITY);
-        primaryStage.setTitle("JavaFx Widget");
-        primaryStage.maximizedProperty().addListener(e ->
-        {
-            properties.setIsStageMaximized(primaryStage.isMaximized());
-            properties.storeValues();
-        });
-        primaryStage.setMaximized(properties.getIsStageMaximized());
+        HBox mainHBox = createBoxAndSetProperties();
+        createScene(mainHBox);
+        bindBoxToSceneAndAddItems(mainHBox);
+        setStageProperties(primaryStage);
+    }
+    
+    private HBox createBoxAndSetProperties ()
+    {
         HBox mainHBox = new HBox();
         mainHBox.getStyleClass().add("pane");
         mainHBox.setPadding(new Insets(10));
+        return mainHBox;
+    }
+    
+    private void createScene (HBox mainHBox)
+    {
         scene = new Scene(mainHBox, properties.getWidth(), properties.getHeight());
         scene.getStylesheets().add("/MainStyle.css");
+    }
+    
+    private void bindBoxToSceneAndAddItems (HBox mainHBox)
+    {
         mainHBox.prefWidthProperty().bind(scene.widthProperty());
         mainHBox.prefHeightProperty().bind(scene.heightProperty());
         mainHBox.getChildren().add(LeftBox
                 .newInstance(mainHBox.prefWidthProperty(), mainHBox.prefHeightProperty()));
         mainHBox.getChildren().add(RightBox
                 .newInstance(mainHBox.prefWidthProperty(), mainHBox.prefHeightProperty()));
+    }
+    
+    private void setStageProperties (Stage primaryStage)
+    {
+        setStageChangeListeners(primaryStage);
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setTitle("JavaFx Widget");
+        addMaximizedListenerToStage(primaryStage);
+        primaryStage.setMaximized(properties.getIsStageMaximized());
         primaryStage.setScene(scene);
         primaryStage.setX(properties.getStageX());
         primaryStage.setY(properties.getStageY());
-        setStageChangeListeners(primaryStage);
         primaryStage.getIcons().add(new Image("widget icon.png"));
         primaryStage.show();
     }
     
+    private void addMaximizedListenerToStage (Stage primaryStage)
+    {
+        primaryStage.maximizedProperty().addListener(e ->
+        {
+            properties.setIsStageMaximized(primaryStage.isMaximized());
+            properties.storeValues();
+        });
+    }
+    
     private void setStageChangeListeners (Stage primaryStage)
     {
-        scene.widthProperty().addListener(e ->
-        {
-            storePropertyValues(primaryStage);
-        });
-        scene.heightProperty().addListener(e ->
-        {
-            storePropertyValues(primaryStage);
-        });
-        primaryStage.xProperty().addListener(e ->
-        {
-            storePropertyValues(primaryStage);
-        });
-        primaryStage.yProperty().addListener(e ->
-        {
-            storePropertyValues(primaryStage);
-        });
+        setSizeListeners(primaryStage);
+        setPositionListeners(primaryStage);
+    }
+    
+    private void setPositionListeners (Stage primaryStage)
+    {
+        primaryStage.xProperty().addListener(e -> storePropertyValues(primaryStage));
+        primaryStage.yProperty().addListener(e -> storePropertyValues(primaryStage));
+    }
+    
+    private void setSizeListeners (Stage primaryStage)
+    {
+        scene.widthProperty().addListener(e -> storePropertyValues(primaryStage));
+        scene.heightProperty().addListener(e -> storePropertyValues(primaryStage));
     }
     
     private void storePropertyValues (Stage primaryStage)
