d4b649c776405c7a9e26ec2b36814132d621e792
Arivan <arivanamin@gmail.com>
2018-01-23 18:34:08 +0300

    create file system box

--- src/main/java/com/arivan/amin/widget/file_system/LinuxFileSystem.java
+++ src/main/java/com/arivan/amin/widget/file_system/LinuxFileSystem.java
@@ -3,14 +3,21 @@ package com.arivan.amin.widget.file_system;
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class LinuxFileSystem implements FileSystem
 {
     private final Logger logger = Logger.getLogger(getClass().getName());
+    private static final Pattern EXTRA_SPACE = Pattern.compile(" {2,}");
     private String data;
     
     private LinuxFileSystem ()
+    {
+        updateFileSystemData();
+    }
+    
+    private void updateFileSystemData ()
     {
         try
         {
@@ -28,12 +35,22 @@ public class LinuxFileSystem implements FileSystem
         return new LinuxFileSystem();
     }
     
+    private FileSystemItem createItemFromLine (String line)
+    {
+        List<String> splitList = List.of(line.split(" "));
+        String percent = splitList.get(4);
+        percent = percent.replace("%", "");
+        return FileSystemItem
+                .newInstance(splitList.get(1), splitList.get(2), splitList.get(3), splitList.get(3),
+                        Double.parseDouble(percent));
+    }
+    
     @Override
     public List<FileSystemItem> partitions ()
     {
-        Stream<String> lines =
-                List.of(data.split("\n")).stream().filter(s -> s.startsWith("/dev/"));
-        // todo extract values from lines
-        return List.of(FileSystemItem.newInstance("/home", "630G", "94G", "505G", 0.16));
+        List<String> lines = List.of(data.split("\n")).stream().filter(s -> s.startsWith("/dev/"))
+                .collect(Collectors.toList());
+        return lines.stream().map(e -> EXTRA_SPACE.matcher(e).replaceAll(" "))
+                .map(this::createItemFromLine).collect(Collectors.toList());
     }
 }
